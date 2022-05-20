94e4f4cb759ba0220019837bce0e1219e51f7563
keys4words <keys4words@gmail.com>
2022-05-20 16:12:11 +0300

    divide frontend dockerfile to prod & dev

--- reactNodePost/frontend/src/App.js
+++ reactNodePost/frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit something in <code>src/App.js</code> and save to reload.
+          Edit something to <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
